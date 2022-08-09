f35a1932eb891ddd3bbb0f4b1846bf0b9493aecd
hongjuns <asws12345@naver.com>
2022-08-09 21:49:50 +0900

    first commit

---
+++ src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL :"https://api.themoviedb.org/3",
+    params : {
+        api_key : "9c88b342784a2461846b9307e253df9f",
+        language : "ko-KR"
+    }
+
+});
+
+export default instance;
