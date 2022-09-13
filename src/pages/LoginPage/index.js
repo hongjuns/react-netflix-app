4f13a5744c11b3cfd351de8f1375b9d74a8ae625
hongjuns <asws12345@naver.com>
2022-09-13 22:14:47 +0900

    로그인 페이지 추가

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -2,6 +2,7 @@ import React ,{ useState } from 'react'
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setToken } from '../../reducer/authSlice';
+import "../../css/Login.css"
 export default function Login() {
 
   const navigate = useNavigate();
@@ -20,6 +21,7 @@ export default function Login() {
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
+    
     let body = {
       id: email,
       pw: password,
@@ -30,24 +32,18 @@ export default function Login() {
   }
 
   return (
-    <div style={{
-        display: 'flex', justifyContent: 'center', alignItems: 'center'
-        , width: '100%', height: '100vh'
-      }}>
-  
-          <form style={{ display: 'flex', flexDirection: 'column' }}
-            onSubmit={onSubmitHandler}
-          >
-              <label>Email</label>
-              <input type="email" value={email} onChange={onEmailHandler}  />  
-              <label>Password</label> 
-              <input type="password" value={password}  onChange={onPasswordHandler} />
-              <br />
-              <button type="submit">
-                Login
-              </button>
-          </form>
-  
+    <div className='login'>
+      <h1 className='login__title'>로그인</h1>
+      <div className='login__group'>
+        <input className='login__group__input' type="text" value={email} onChange={onEmailHandler}/>
+        <label className='login__group__label'>Email or phone number</label>
       </div>
+      <div className='login__group'>
+        <input className='login__group__input' type="password" value={password} onChange={onPasswordHandler}/>
+        <label className='login__group__label'>Password</label>
+      </div>
+      <button className='login__sign-in' type="button" onClick={onSubmitHandler}>Sign In</button>
+      <div className='login__secondary-cta'><p className='login__secondary-cta__text' href="#">Remember me</p><p className='login__secondary-cta__text login__secondary-cta__text--need-help' href="#">Need help?</p></div>
+    </div>
   )
 }
